f3303aaa9e3117907266cb0caa330d433f7ec135
Francisco <fmalcab1801@ieszaidinvergeles.org>
2026-09-25 12:18:56 +0200

    ejercicio5

--- Tipo de datos simples/Ejercicio5.py
+++ Tipo de datos simples/Ejercicio5.py
@@ -1,4 +1,4 @@
 hora = int(input("Dime el numero de horas que trabajas: "))
 coste = int(input("Dime el coste por hora: "))
 cuenta = hora*coste
-print(f"El coste total es: {cuenta}")               
+print(f"El coste total es: {cuenta}")
