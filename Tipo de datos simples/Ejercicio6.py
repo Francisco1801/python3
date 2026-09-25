8f07ceed61e20ed2e932abe82b1e41264921e6a3
Francisco <fmalcab1801@ieszaidinvergeles.org>
2026-09-25 12:36:35 +0200

    ejercicio6

---
+++ Tipo de datos simples/Ejercicio6.py
@@ -0,0 +1,3 @@
+numEntero = int(input("Dime un numero entero positivo: "))
+NumSuma = numEntero * (numEntero + 1) / 2
+print (f"La suma de los {numEntero} primeros numeros enteros positivos es: {NumSuma}")
